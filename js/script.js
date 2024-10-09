fb381b89809bb06edcaad0d9c7f868cb200bf8a4
AkhdaanLogics <akhdaan@students.amikom.ac.id>
2024-10-09 19:43:47 +0700

    validasi

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log('Javascript Berhasil Dikaitkan');
+
+function convert() {
+    const inputSuhu = document.getElementById('celsius').value;
+    
+    if (inputSuhu == '') {
+        alert('Mohon Isi Suhu Dulu');
+    }
+}
