896eee509ef5e054c1e66e71d4ce9d33e16100b3
AkhdaanLogics <akhdaan@students.amikom.ac.id>
2024-10-09 20:31:02 +0700

    rumus

--- js/script.js
+++ js/script.js
@@ -2,8 +2,29 @@ console.log('Javascript Berhasil Dikaitkan');
 
 function convert() {
     const inputSuhu = document.getElementById('celsius').value;
-    
+
     if (inputSuhu == '') {
         alert('Mohon Isi Suhu Dulu');
     }
+
+    else {
+        calculate(inputSuhu);
+    }
+}
+
+function calculate(value) {
+    if (value !== '') {
+        let result = (value * 9/5) + 32;
+        document.getElementById('fahrenheit').value = result + ' °F';
+        document.getElementById('rumus').innerText = value + '°C * (9/5) + 32 = ' + result + ' °F';
+        document.getElementById('calculation').style.display = 'block';
+    }
+}
+
+
+function reset() {
+    document.getElementById('celsius').value = '';
+    document.getElementById('fahrenheit').value = '';
+    document.getElementById('rumus').innerText = '';
+    document.getElementById('calculation').style.display = 'none';
 }
